d8ed8e6b5db630220f3679edfa84c2be960632ee
MrPRometheus <amir505553amir@gmail.com>
2023-10-23 15:29:27 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 // matching all API routes
-                source: "https://ep-dawn-shape-65068163.eu-central-1.postgres.vercel-storage.com*",
+                source: "/ep-dawn-shape-65068163.eu-central-1.postgres.vercel-storage.com*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
